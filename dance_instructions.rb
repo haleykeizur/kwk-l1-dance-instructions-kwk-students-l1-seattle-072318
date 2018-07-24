129cc52a0268e783525f67aea4ed711a85abc2b0
haleykeizur <haleykz@yahoo.com>
2018-07-24 16:50:08 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -9,4 +9,5 @@ end
 def skip_step_left
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
-  puts ""
+  puts "Left, Right, Left and Left"
+end
